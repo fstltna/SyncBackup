e4a4bcfd27964156577a57ee4895dcdd909de10d
fstltna <fstltna@yahoo.com>
2019-06-28 17:14:24 -0700

    Now allows you to offsite backup your BBS

--- syncbackup.pl
+++ syncbackup.pl
@@ -4,11 +4,80 @@
 my $SYNCDIR = "/sbbs";
 my $BACKUPDIR = "/root/backups";
 my $TARCMD = "/bin/tar czf";
-my $VERSION = "1.1";
+my $VERSION = "2.0";
+
+# Init file data
+my $MySettings = "$ENV{'HOME'}/.sbackuprc";
+my $BACKUPUSER = "";
+my $BACKUPPASS = "";
+my $BACKUPSERVER = "";
+my $BACKUPPATH = "";
+my $DEBUG_MODE = "off";
 
 #-------------------
 # No changes below here...
 #-------------------
+
+sub ReadConfigFile
+{
+	# Check for config file
+	if (-f $MySettings)
+	{
+		# Read in settings
+		open (my $FH, "<", $MySettings) or die "Could not read default file '$MySettings' $!";
+		while (<$FH>)
+		{
+			chop();
+			my ($Command, $Setting) = split(/=/, $_);
+			if ($Command eq "backupuser")
+			{
+				$BACKUPUSER = $Setting;
+			}
+			if ($Command eq "backuppass")
+			{
+				$BACKUPPASS = $Setting;
+			}
+			if ($Command eq "backupserver")
+			{
+				$BACKUPSERVER = $Setting;
+			}
+			if ($Command eq "backuppath")
+			{
+				$BACKUPPATH = $Setting;
+			}
+			if ($Command eq "debugmode")
+			{
+				$DEBUG_MODE = $Setting;
+			}
+		}
+		close($FH);
+	}
+	else
+	{
+		# Store defaults
+		open (my $FH, ">", $MySettings) or die "Could not create default file '$MySettings' $!";
+		print $FH "backupuser=\n";
+		print $FH "backuppass=\n";
+		print $FH "backupserver=\n";
+		print $FH "backuppath=\n";
+		print $FH "debugmode=off\n";
+		close($FH);
+	}
+}
+
+sub PrintDebugCommand
+{
+	if ($DEBUG_MODE eq "off")
+	{
+		return;
+	}
+	my $PassedString = shift;
+	print "About to run:\n$PassedString\n";
+	print "Press Enter To Run This:";
+	my $entered = <STDIN>;
+}
+
+ReadConfigFile();
 
 print "SyncBackup - back up your Synchronet BBS - version $VERSION\n";
 print "======================================================\n";
@@ -42,5 +111,13 @@ if (-f "$BACKUPDIR/syncbackup-1.tgz")
 }
 print "Done\nCreating Backup: ";
 system("$TARCMD $BACKUPDIR/syncbackup-1.tgz $SYNCDIR --exclude='/sbbs/ctrl/localspy*.sock' --exclude='/sbbs/ctrl/status.sock'");
+if ($BACKUPSERVER ne "")
+{
+	print "Offsite backup requested\n";
+	print "Copying $BACKUPDIR/syncbackup-1.tgz to $BACKUPSERVER:$BACKUPPORT\n";
+	print ("sshpass -p \"$BACKUPPASS\" scp $BACKUPDIR/syncbackup-1.tgz $BACKUPUSER\@$BACKUPSERVER:$BACKUPPATH\n");
+	system ("sshpass -p \"$BACKUPPASS\" scp $BACKUPDIR/syncbackup-1.tgz $BACKUPUSER\@$BACKUPSERVER:$BACKUPPATH");
+}
+
 print("Done!\n");
 exit 0;
